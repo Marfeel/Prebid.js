bd6f2982beb425f0589377e2243858b1e00117ec
Alex Alvarez Chinchilla <alex.alvarez@marfeel.com>
2018-05-21 16:17:37 +0200

    Fix ix adapter referrer

--- modules/ixBidAdapter.js
+++ modules/ixBidAdapter.js
@@ -205,17 +205,11 @@ export const spec = {
 
     r.imp = bannerImps;
     r.site = {};
-    r.site.page = utils.getTopWindowUrl();
+    r.site.page = page || utils.getTopWindowUrl();
     r.ext = {};
     r.ext.source = 'prebid';
 
-    const referrer = validBidRequests[0].params.referrer;
-
-    if (referrer) {
-      r.site.ref = referrer;
-    } else {
-      r.site.ref = utils.getTopWindowReferrer();
-    }
+    r.site.ref = utils.getTopWindowReferrer();
 
     // Apply GDPR information to the request if GDPR is enabled.
     if (options) {
